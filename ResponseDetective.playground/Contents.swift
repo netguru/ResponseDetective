e964be1c1a292ce178a101036bac1c22d05d032e
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-06-15 13:41:35 +0200

    Add wip playground content for debugging
    [#95964218]

--- ResponseDetective.playground/Contents.swift
+++ ResponseDetective.playground/Contents.swift
@@ -11,3 +11,41 @@ import XCPlayground
 // Enable indefinite execution so that requests complete before the playground
 // is killed.
 XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
+
+// /////////////////////////////////////////////////////////////////////////////
+
+struct PlaygroundInterceptor: RequestInterceptorType, ResponseInterceptorType {
+
+	func canInterceptRequest(request: NSURLRequest) -> Bool {
+		return true
+	}
+
+	func interceptRequest(request: NSURLRequest) {
+		print(request)
+	}
+
+	func canInterceptResponse(response: NSHTTPURLResponse) -> Bool {
+		return true
+	}
+
+	func interceptResponse(response: NSHTTPURLResponse, _ data: NSData) {
+		print((response, data))
+	}
+
+	func interceptResponseError(response: NSHTTPURLResponse?, _ error: NSError) {
+		print((response, error))
+	}
+
+}
+
+let requestToken = InterceptingProtocol.registerRequestInterceptor(PlaygroundInterceptor())
+let responseToken = InterceptingProtocol.registerResponseInterceptor(PlaygroundInterceptor())
+
+let session = NSURLSession(configuration: { () -> NSURLSessionConfiguration in
+	let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+	configuration.protocolClasses = [InterceptingProtocol.self]
+	return configuration
+}())
+
+let request = NSURLRequest(URL: NSURL(string: "http://httpbin.org/get")!)
+session.dataTaskWithRequest(request).resume()
