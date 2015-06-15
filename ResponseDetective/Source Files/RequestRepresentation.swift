26eec47a20700489d7a59dbfe8e8a3f0aa4a5568
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-06-15 19:00:22 +0200

    Add request and response representations
    [#95960426]

---
+++ ResponseDetective/Source Files/RequestRepresentation.swift
@@ -0,0 +1,58 @@
+//
+//  RequestRepresentation.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a request.
+public struct RequestRepresentation {
+
+	/// Request method.
+	public let method: String
+
+	/// Request URL string.
+	public let url: String
+
+	/// Request headers, represented by strings.
+	public let headers: [String: String]
+
+	/// Request content type.
+	public var contentType: String {
+		return headers["Content-Type"] ?? "application/octet-stream"
+	}
+
+	/// Request body data.
+	public let body: NSData?
+
+	/// Request body UTF-8 string.
+	public var bodyString: String? {
+		return flatMap(body) { NSString(data: $0, encoding: NSUTF8StringEncoding) } as String?
+	}
+
+	/// Initializes the receiver with an instance of NSURLRequest.
+	///
+	/// :param: request The foundation NSSURLRequest object.
+	///
+	/// :returns: An initialized receiver or nil if an instance sould not be
+	/// created using the given request.
+	public init?(request: NSURLRequest) {
+		if let method = request.HTTPMethod, let url = request.URL?.absoluteString {
+			self.method = method
+			self.url = url
+			self.headers = map(request.allHTTPHeaderFields) { headers in
+				reduce(headers, [:]) { (var initial, element) in
+					if let key = element.0 as? String, value = element.1 as? String {
+						initial[key] = value
+					}
+					return initial
+				}
+			} ?? [:]
+			self.body = request.HTTPBody?.copy() as! NSData?
+		} else {
+			return nil
+		}
+	}
+
+}
---
+++ ResponseDetective/Source Files/ResponseRepresentation.swift
@@ -0,0 +1,57 @@
+//
+//  ResponseRepresentation.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a response.
+public struct ResponseRepresentation {
+
+	/// Response status code.
+	public let statusCode: Int
+
+	/// Response URL.
+	public let url: String
+
+	/// Response headers
+	public let headers: [String: String]
+
+	/// Response content type.
+	public var contentType: String {
+		return headers["Content-Type"] ?? "application/octet-stream"
+	}
+
+	/// Response body data.
+	public let body: NSData
+
+	/// Response body UTF-8 string.
+	public var bodyString: String? {
+		return NSString(data: body, encoding: NSUTF8StringEncoding) as String?
+	}
+
+	/// Initializes the receiver with an instance of NSHTTPURLResponse.
+	///
+	/// :param: response The foundation NSHTTPURLResponse object.
+	/// :param: data The data assocciated with the response.
+	///
+	/// :returns: An initialized receiver or nil if an instance sould not be
+	/// created using the given response.
+	public init?(response: NSHTTPURLResponse, data: NSData) {
+		if let url = response.URL?.absoluteString {
+			self.statusCode = response.statusCode
+			self.url = url
+			self.headers = reduce(response.allHeaderFields, [:]) { (var initial, element) in
+				if let key = element.0 as? String, value = element.1 as? String {
+					initial[key] = value
+				}
+				return initial
+			}
+			self.body = data
+		} else {
+			return nil
+		}
+	}
+
+}
