2a0b1f22f8cd5cef6a3130bfb1c35e65dbcb6e86
Aleksander Popko <aleksander.popko@netguru.pl>
2015-06-24 14:45:53 +0200

    Improve InterceptorType documentation comments.
    [#97705514]

--- ResponseDetective/Source Files/InterceptorType.swift
+++ ResponseDetective/Source Files/InterceptorType.swift
@@ -7,12 +7,12 @@
 import Foundation
 
 /// Instances of conforming types can be registered in the intercepting
-/// NSURLProtocol and used to intercept NSURLRequests and NSHTTPURLResponses.
+/// NSURLProtocol and used to intercept NSURLRequests or NSHTTPURLResponses.
 @objc public protocol InterceptorType {
 	
 	/// Whether the interceptor can intercept and process the outgoing request or the incoming response.
 	///
-	/// :param: content The request or the response in question.
+	/// :param: content The request or the the response in question.
 	///
 	/// :returns: A boolean indicating whether the request or the response should be
 	/// intercepted and processed.
@@ -22,13 +22,13 @@ import Foundation
 	/// effects should be executed asynchronously, so that the response doesn't
 	/// get blocked.
 	///
-	/// :param: response The response to be processed.
-	/// :param: data The data which arrived with the response.
+	/// :param: content The request or the the response to be processed.
+	/// :param: data The data which arrived with the response. Optional parameter, used only in response interceptors.
 	func intercept(content: AnyObject, data: NSData?)
 	
-	/// Intercepts and processes the incoming response error. Used only in response interceptors. Preferably, all
+	/// Intercepts and processes the incoming response error. Preferably, all
 	/// side effects should be executed asynchronously, so that the response
-	/// doesn't get blocked.
+	/// doesn't get blocked. Optional func, used only in response interceptors.
 	///
 	/// :param: response The response received along the error (if any).
 	/// :param: error The associated error.
