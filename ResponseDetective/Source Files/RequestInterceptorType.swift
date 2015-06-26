c567401e938b0219550d3e85332f5e98043510cd
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-06-26 18:19:15 +0200

    Split interceptor types in own files and use representations
    [#95960426]

---
+++ ResponseDetective/Source Files/RequestInterceptorType.swift
@@ -0,0 +1,29 @@
+//
+//  RequestInterceptorType.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+/// Instances of conforming types can be registered in the intercepting
+/// NSURLProtocol and used to intercept NSURLRequests.
+public protocol RequestInterceptorType {
+
+	/// Whether the interceptor can intercept and process the outgoing request.
+	///
+	/// :param: request The request in question.
+	///
+	/// :returns: A boolean indicating whether the request should be
+	/// intercepted and processed.
+	func canInterceptRequest(request: RequestRepresentation) -> Bool
+
+	/// Intercepts and processes the outgoing request. Preferably, all side
+	/// effects should be executed asynchronously, so that the request doesn't
+	/// get blocked.
+	///
+	/// :param: request The request to be processed.
+	func interceptRequest(request: RequestRepresentation)
+
+}
+
---
+++ ResponseDetective/Source Files/ResponseInterceptorType.swift
@@ -0,0 +1,26 @@
+//
+//  ResponseInterceptorType.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+/// Instances of conforming types can be registered in the intercepting
+/// NSURLProtocol and used to intercept NSHTTPURLResponses.
+public protocol ResponseInterceptorType {
+
+	/// Whether the interceptor can intercept and process the incoming response.
+	///
+	/// :param: response The response in question.
+	///
+	/// :returns: A boolean indicating whether the response should be
+	/// intercepted and processed.
+	func canInterceptResponse(response: ResponseRepresentation) -> Bool
+
+	/// Intercepts and processes the incoming response. Preferably, all side
+	/// effects should be executed asynchronously, so that the response doesn't
+	/// get blocked.
+	///
+	/// :param: response The response to be processed.
+	func interceptResponse(response: ResponseRepresentation)
+
+}
