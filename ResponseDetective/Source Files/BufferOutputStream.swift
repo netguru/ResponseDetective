d87071c80b5daaad24d775acdcb06801795bc916
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-06-26 18:18:54 +0200

    Split output stream types in own files
    [#95960426]

---
+++ ResponseDetective/Source Files/BufferOutputStream.swift
@@ -0,0 +1,19 @@
+//
+//  BufferOutputStream.swift
+//  
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+/// An output stream which adds its data to a buffer array.
+public final class BufferOutputStream: OutputStreamType {
+
+	/// The buffer array containing all the messages.
+	public private(set) var buffer: [String] = []
+
+	// MARK: OutputStreamType implementation
+
+	public func write(string: String) {
+		buffer.append(string)
+	}
+	
+}
---
+++ ResponseDetective/Source Files/NSLogOutputStream.swift
@@ -0,0 +1,20 @@
+//
+//  NSLogOutputStream.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+/// An output stream which prints its data right to the console using
+/// Foundation's NSLog function. This stream might be used instead of Println
+/// stream if you'd like your messages to appear in Apple System Log as well.
+public struct NSLogOutputStream: OutputStreamType {
+
+	// MARK: OutputStreamType implementation
+
+	public func write(string: String) {
+		NSLog("%@", string)
+	}
+	
+}
