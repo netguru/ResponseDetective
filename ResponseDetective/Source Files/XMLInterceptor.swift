93b15e71793a8f03e5f4ad7c5e19b5a250c640d6
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-07-02 18:23:45 +0200

    Add XML request and response interceptor
    [#97764480]

---
+++ ResponseDetective/Source Files/XMLInterceptor.swift
@@ -0,0 +1,106 @@
+//
+//  XMLInterceptor.swift
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+public final class XMLInterceptor {
+
+	/// The output stream used by the interceptor.
+	public private(set) var outputStream: OutputStreamType
+
+	// The acceptable content types.
+	private let acceptableContentTypes = [
+		"application/xml",
+		"text/xml"
+	]
+
+	// MARK: Initialization
+
+	/// Initializes the interceptor with a output stream.
+	///
+	/// :param: outputStream The output stream to be used.
+	public init(outputStream: OutputStreamType) {
+		self.outputStream = outputStream
+	}
+
+	/// Initializes the interceptor with a Println output stream.
+	public convenience init() {
+		self.init(outputStream: PrintlnOutputStream())
+	}
+
+	// MARK: Prettifying
+
+	/// Prettifies the JSON string.
+	///
+	/// :param: data The XML string to prettify.
+	///
+	/// :returns: A prettified XML string.
+	private func prettifyXMLString(string: String) -> String? {
+		return rdv_prettifyXMLString(string)
+	}
+
+	/// Prettifies the XML data.
+	///
+	/// :param: stream The XML data to prettify.
+	///
+	/// :returns: A prettified XML string.
+	private func prettifyXMLData(data: NSData) -> String? {
+		return flatMap(flatMap(data, {
+			NSString(data: $0, encoding: NSUTF8StringEncoding) as String?
+		}), {
+			self.prettifyXMLString($0)
+		})
+	}
+
+}
+
+// MARK: -
+
+extension XMLInterceptor: RequestInterceptorType {
+
+	// MARK: RequestInterceptorType implementation
+
+	public func canInterceptRequest(request: RequestRepresentation) -> Bool {
+		return map(request.contentType) {
+			contains(self.acceptableContentTypes, $0)
+		} ?? false
+	}
+
+	public func interceptRequest(request: RequestRepresentation) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+			if let jsonString = flatMap(request.bodyData, { self.prettifyXMLData($0) }) {
+				dispatch_async(dispatch_get_main_queue()) {
+					self.outputStream.write(jsonString)
+				}
+			}
+		}
+	}
+
+}
+
+// MARK: -
+
+extension XMLInterceptor: ResponseInterceptorType {
+
+	// MARK: ResponseInterceptorType implementation
+
+	public func canInterceptResponse(response: ResponseRepresentation) -> Bool {
+		return map(response.contentType) {
+			contains(self.acceptableContentTypes, $0)
+		} ?? false
+	}
+
+	public func interceptResponse(response: ResponseRepresentation) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+			if let jsonString = flatMap(response.bodyData, { self.prettifyXMLData($0) }) {
+				dispatch_async(dispatch_get_main_queue()) {
+					self.outputStream.write(jsonString)
+				}
+			}
+		}
+	}
+	
+}
