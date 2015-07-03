be57d3b90975722e919458202f2090620a415c5a
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-07-03 15:11:32 +0200

    Simplify xml prettification code
    [#97764522]

--- ResponseDetective/Source Files/XMLPrettifying.m
+++ ResponseDetective/Source Files/XMLPrettifying.m
@@ -8,23 +8,11 @@
 #import "XMLPrettifying.h"
 
 NSString * __nullable rdv_prettifyXMLString(NSString * __nonnull string) {
-	xmlNodePtr rootNode = ^{
-		const char *cString = string.UTF8String;
-		xmlDocPtr document = xmlReadMemory(cString, ((int)(strlen(cString))), NULL, NULL, XML_PARSE_NOCDATA | XML_PARSE_NOBLANKS);
-		xmlNodePtr rootNode = xmlDocGetRootElement(document);
-		xmlNodePtr recursiveRootNode = xmlCopyNode(rootNode, 1);
-		xmlFreeDoc(document);
-		return recursiveRootNode;
-	}();
-	xmlBufferPtr buffer = xmlBufferCreate();
-	int result = xmlNodeDump(buffer, NULL, rootNode, 0, 1);
-	NSString *prettyString = (result > -1) ? ^{
-		return [[NSString alloc]
-			initWithBytes:xmlBufferContent(buffer)
-			length:((NSUInteger)(xmlBufferLength(buffer)))
-			encoding:NSUTF8StringEncoding
-		];
-	}() : nil;
-	xmlBufferFree(buffer);
-	return [prettyString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	const char *memory = string.UTF8String;
+	xmlDocPtr document = xmlReadMemory(memory, ((int)(strlen(memory))), NULL, NULL, XML_PARSE_NOCDATA | XML_PARSE_NOBLANKS);
+	xmlChar *buffer = NULL; int bufferLength = 0;
+	xmlDocDumpFormatMemory(document, &buffer, &bufferLength, 1);
+	NSString *result = [[NSString alloc] initWithBytes:buffer length:bufferLength encoding:NSUTF8StringEncoding];
+	xmlFree(buffer);
+	return result;
 }
