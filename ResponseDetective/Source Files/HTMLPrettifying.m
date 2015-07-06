148ff56b30fddf7491fc0c99fa933bcee7b8d7d0
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-07-06 11:50:14 +0200

    Review fixes
    [#97766840]
    
    [be57d3b90975722e919458202f2090620a415c5a]
    [3e6d833852d9bf9a1bf9166f9be7e1174fa58b58]

--- ResponseDetective/Source Files/HTMLPrettifying.m
+++ ResponseDetective/Source Files/HTMLPrettifying.m
@@ -4,17 +4,16 @@
 //  Created by Adrian Kashivskyy on 03.07.2015.
 //
 
-#import <libxml/tree.h>
-#import <libxml/HTMLparser.h>
 #import <libxml/HTMLtree.h>
-#import "XMLPrettifying.h"
+#import "HTMLPrettifying.h"
 
 NSString * __nullable rdv_prettifyHTMLString(NSString * __nonnull string) {
 	const char *memory = string.UTF8String;
 	htmlDocPtr document = htmlReadMemory(memory, ((int)(strlen(memory))), NULL, NULL, HTML_PARSE_NOBLANKS);
-	xmlChar *buffer = NULL; int bufferSize = 0;
-	htmlDocDumpMemoryFormat(document, &buffer, &bufferSize, 1);
-	NSString *result = [[NSString alloc] initWithBytes:buffer length:bufferSize encoding:NSUTF8StringEncoding];
+	xmlChar *buffer = NULL;
+	int bufferLength = 0;
+	htmlDocDumpMemoryFormat(document, &buffer, &bufferLength, 1);
+	NSString *result = [[NSString alloc] initWithBytes:buffer length:bufferLength encoding:NSUTF8StringEncoding];
 	xmlFree(buffer);
 	return result;
 }
--- ResponseDetective/Source Files/XMLPrettifying.m
+++ ResponseDetective/Source Files/XMLPrettifying.m
@@ -1,7 +1,7 @@
 //
 //  XMLPrettifying.m
 //
-//  Created by Adrian Kashivskyy on 02.07.2015.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
 //
 
 #import <libxml/tree.h>
@@ -10,7 +10,8 @@
 NSString * __nullable rdv_prettifyXMLString(NSString * __nonnull string) {
 	const char *memory = string.UTF8String;
 	xmlDocPtr document = xmlReadMemory(memory, ((int)(strlen(memory))), NULL, NULL, XML_PARSE_NOCDATA | XML_PARSE_NOBLANKS);
-	xmlChar *buffer = NULL; int bufferLength = 0;
+	xmlChar *buffer = NULL;
+	int bufferLength = 0;
 	xmlDocDumpFormatMemory(document, &buffer, &bufferLength, 1);
 	NSString *result = [[NSString alloc] initWithBytes:buffer length:bufferLength encoding:NSUTF8StringEncoding];
 	xmlFree(buffer);
