140a0277fe891d0994f0a58576836fef6057f8a4
Aleksander Popko <aleksander.popko@netguru.pl>
2015-07-07 18:04:58 +0200

    [WIP] Add initial XMLInterceptorSpec.
    [#97766840]

---
+++ ResponseDetective Tests/Source Files/XMLInterceptorSpec.swift
@@ -0,0 +1,53 @@
+//
+//  XMLInterceptorSpec.swift
+//  
+//
+//  Created by Aleksander Popko on 07.07.2015.
+//
+//
+
+import Foundation
+import Nimble
+import ResponseDetective
+import Quick
+
+
+class XMLInterceptorSpec: QuickSpec {
+	
+	override func spec() {
+		
+		describe("XMLInterceptor") {
+			
+			var stream: BufferOutputStream!
+			var sut: XMLInterceptor!
+			
+			beforeEach {
+				stream = BufferOutputStream()
+				sut = XMLInterceptor(outputStream: stream)
+			}
+			
+			it("should be able to intercept xml requests") {
+				let request = RequestRepresentation({
+					var mutableRequest = NSMutableURLRequest()
+					mutableRequest.URL = NSURL(string: "https://httpbin.org/get")!
+					mutableRequest.addValue("application/xml", forHTTPHeaderField: "Content-Type");
+					return mutableRequest
+					}())!
+				expect(sut.canInterceptRequest(request)).to(beTrue())
+			}
+			
+			it("should be able to intercept xml responses") {
+				let response = ResponseRepresentation(NSHTTPURLResponse(
+					URL: NSURL(string: "https://httpbin.org/get")!,
+					statusCode: 200,
+					HTTPVersion: "HTTP/1.1",
+					headerFields: [
+						"Content-Type": "application/xml"
+					]
+					)!, nil)!
+				expect(sut.canInterceptResponse(response)).to(beTrue())
+			}
+		}
+	}
+
+}
