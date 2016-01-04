d2121610175b061a11ba342ea2eaf40303c15be9
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-01-04 17:27:19 +0100

    Add initial response detective implementation

---
+++ ResponseDetective/Source Files/ResponseDetective.swift
@@ -0,0 +1,39 @@
+//
+//  ResponseDetective.swift
+//
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+//  Licensed under the MIT License.
+//
+
+import Foundation
+
+@objc(RDVResponseDetective) public final class ResponseDetective: NSObject {
+	
+	/// An output facility for reporting requests, responses and errors.
+	public let outputFacility: OutputFacility
+	
+	/// A URL protocol instance to be injected into URL sessions.
+	public let URLProtocol: NSURLProtocol
+	
+	/// A URL session delegate to be injected into URL sessions.
+	public let URLSessionDelegate: NSURLSessionDelegate
+	
+	/// Initializes the ResponseDetective instance.
+	///
+	/// - Parameters:
+ 	///     - outputFacility: An output facility.
+	///
+	/// - Returns: An initialized instance of the receiver.
+	public init(outputFacility: OutputFacility) {
+		self.outputFacility = outputFacility;
+	}
+	
+	/// Initializes the ResponseDetective instance with a default configuration.
+	///
+	/// - Returns: An initialized instance of the receiver.
+	public override convenience init() {
+		self.init(outputFacility: ConsoleOutputFacility())
+	}
+	
+}
