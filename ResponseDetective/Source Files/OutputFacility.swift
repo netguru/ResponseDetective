b88d292c2a37715c4b259d9c14f54ea28ed9420f
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-01-04 16:48:14 +0100

    Add output facility protocol

---
+++ ResponseDetective/Source Files/OutputFacility.swift
@@ -0,0 +1,33 @@
+//
+//  OutputFacility.swift
+//
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+//  Licensed under the MIT License.
+//
+
+import Foundation
+
+/// Represents an output facility which is capable of displaying requests,
+/// responses and errors.
+@objc(RFVOutputFacility) public protocol OutputFacility {
+	
+	/// Outputs a request representation.
+	///
+	/// - Parameters:
+	///     - request: The request representation to output.
+	func outputRequestRepresentation(request: RequestRepresentation)
+	
+	/// Outputs a response representation.
+	///
+	/// - Parameters:
+	///     - response: The response representation to output.
+	func outputResponseRepresentation(response: ResponseRepresentation)
+	
+	/// Outputs an error representation.
+	///
+	/// - Parameters:
+	///     - error: The error representation to output.
+	func outputErrorRepresentation(error: ErrorRepresentation)
+	
+}
