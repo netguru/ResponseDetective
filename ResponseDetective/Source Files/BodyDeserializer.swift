df19ef738bf55356c23ba1c0985c049766c72a98
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-01-07 16:52:39 +0100

    Add body deserializers

---
+++ ResponseDetective/Source Files/BodyDeserializer.swift
@@ -0,0 +1,26 @@
+//
+//  HTTPBodyDeserializer.swift
+//
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+//  Licensed under the MIT License.
+//
+
+import Foundation
+
+/// Represents a body deserializer which is able to deserialize raw body data
+/// into a human-readable string which will be logged as the request's or
+/// response's body.
+@objc(RDVBodyDeserializer) public protocol BodyDeserializer {
+	
+	/// Deserializes the body.
+	///
+	/// - Parameters:
+	///     - body: The HTTP body.
+	///
+	/// - Throws: An error which occured during deserialization.
+	///
+	/// - Returns: A deserialized representation of the body.
+	func deserializeBody(body: NSData) throws -> String
+	
+}
