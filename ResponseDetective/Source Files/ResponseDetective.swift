86645a831305fc03634d7f50781b9210c04e43be
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-01-07 16:53:18 +0100

    Improve ResponseDetective class, make it static, ugh

--- ResponseDetective/Source Files/ResponseDetective.swift
+++ ResponseDetective/Source Files/ResponseDetective.swift
@@ -10,24 +10,94 @@ import Foundation
 
 @objc(RDVResponseDetective) public final class ResponseDetective: NSObject {
 	
-	/// An output facility for reporting requests, responses and errors.
-	public let outputFacility: OutputFacility
-	
-	/// Initializes the ResponseDetective instance.
-	///
-	/// - Parameters:
- 	///     - outputFacility: An output facility.
-	///
-	/// - Returns: An initialized instance of the receiver.
-	public init(outputFacility: OutputFacility) {
-		self.outputFacility = outputFacility;
+	/// Whether ResponseDetective is enabled.
+	public static var enabled: Bool = false {
+		didSet {
+			if enabled {
+				NSURLProtocol.registerClass(URLProtocolClass)
+			} else {
+				NSURLProtocol.unregisterClass(URLProtocolClass)
+			}
+		}
 	}
 	
-	/// Initializes the ResponseDetective instance with a default configuration.
+	/// An output facility for reporting requests, responses and errors.
+	public static var outputFacility: OutputFacility = ConsoleOutputFacility()
+	
+	/// A class of the URL protocol used to intercept requests.
+	public static let URLProtocolClass: AnyClass = URLProtocol.self
+	
+	/// A storage for request predicates.
+	private static var requestPredicates: [NSPredicate] = []
+	
+	/// Body deserializers stored by a supported content type.
+	private static var bodyDeserializers: [String: BodyDeserializer] = [:]
+	
+	/// Resets the ResponseDetective mutable state.
+	public static func reset() {
+		enabled = false
+		outputFacility = ConsoleOutputFacility()
+		requestPredicates = []
+	}
+	
+	/// Ignores requests matching the given predicate. The predicate will be
+	/// evaluated with an instance of NSURLRequest.
 	///
-	/// - Returns: An initialized instance of the receiver.
-	public override convenience init() {
-		self.init(outputFacility: ConsoleOutputFacility())
+	/// - Parameters:
+	///     - predicate: A predicate for matching a request. If the predicate
+	///     evaluates to `false`, the request is not intercepted.
+	public static func ignoreRequestsMatchingPredicate(predicate: NSPredicate) {
+		requestPredicates.append(predicate)
+	}
+	
+	/// Checks whether the given request can be incercepted.
+	///
+	/// - Parameters:
+	///     - request: The request to check.
+	///
+	/// - Returns: `true` if request can be intercepted, `false` otherwise.
+	public static func canIncerceptRequest(request: NSURLRequest) -> Bool {
+		return enabled && requestPredicates.reduce(true) {
+			return $0 && !$1.evaluateWithObject(request)
+		}
+	}
+	
+	/// Registers a body deserializer.
+	///
+	/// - Parameters:
+	///     - deserializer: The deserializer to register.
+	///     - contentType: The supported content type.
+	public static func registerBodyDeserializer(deserializer: BodyDeserializer, forContentType contentType: String) {
+		bodyDeserializers[contentType] = deserializer
+	}
+	
+	/// Registers a body deserializer.
+	///
+	/// - Parameters:
+	///     - deserializer: The deserializer to register.
+	///     - contentTypes: The supported content types.
+	public static func registerBodyDeserializer(deserializer: BodyDeserializer, forContentTypes contentTypes: [String]) {
+		for contentType in contentTypes {
+			registerBodyDeserializer(deserializer, forContentType: contentType)
+		}
+	}
+	
+	/// Deserializes a HTTP body into a string.
+	///
+	/// - Parameters:
+	///     - body: The body to deserialize.
+	///     - contentType: The content type of the body.
+	///
+	/// - Throws: Rethrows error thrown by a deserializer.
+	///
+	/// - Returns: A deserialized body or `nil` if no serializer is capable of
+	/// deserializing body with the given content type.
+	public static func deserializeBody(body: NSData, contentType: String) throws -> String? {
+		if let deserializer = bodyDeserializers[contentType] {
+			return try deserializer.deserializeBody(body)
+		} else {
+			return nil
+		}
 	}
 	
 }
