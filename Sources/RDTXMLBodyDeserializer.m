a8fb918b5dda37e71589da1cefe83b1c4861a227
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-05 19:19:53 +0200

    Add xml body deserializer

---
+++ Sources/RDTXMLBodyDeserializer.m
@@ -0,0 +1,25 @@
+//
+// RDTXMLBodyDeserializer.m
+//
+// Copyright (c) 2016 Netguru Sp. z o.o. All rights reserved.
+// Licensed under the MIT License.
+//
+
+#import <libxml/tree.h>
+#import "RDTXMLBodyDeserializer.h"
+
+@implementation RDTXMLBodyDeserializer
+
+- (nullable NSString *)deserializeBody:(nonnull NSData *)body {
+	NSString *string = [[NSString alloc] initWithData:body encoding:NSUTF8StringEncoding];
+	const char *memory = string.UTF8String;
+	xmlDocPtr document = xmlReadMemory(memory, ((int)(strlen(memory))), NULL, NULL, XML_PARSE_NOCDATA | XML_PARSE_NOBLANKS);
+	xmlChar *buffer = NULL;
+	int bufferLength = 0;
+	xmlDocDumpFormatMemory(document, &buffer, &bufferLength, 1);
+	NSString *result = [[NSString alloc] initWithBytes:buffer length:bufferLength encoding:NSUTF8StringEncoding];
+	xmlFree(buffer);
+	return [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+@end
