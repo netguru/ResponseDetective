5f2d956f85e538af7c4b031f6b3f7e35d7a3638f
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-06 18:22:10 +0200

    Add global foundation import to umbrella header

--- Support/ResponseDetective.h
+++ Support/ResponseDetective.h
@@ -11,6 +11,8 @@ extern double ResponseDetectiveVersionNumber;
 /// Project version string for ResponseDetective.
 extern const unsigned char ResponseDetectiveVersionString[];
 
+@import Foundation;
+
 #import <ResponseDetective/RDTBodyDeserializer.h>
 #import <ResponseDetective/RDTXMLBodyDeserializer.h>
 #import <ResponseDetective/RDTHTMLBodyDeserializer.h>
