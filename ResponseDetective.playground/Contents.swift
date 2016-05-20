a033a9998d6e352f36467011ce359c3c6d1bb050
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-20 14:56:59 +0200

    Add vertical spaces in playground source

--- ResponseDetective.playground/Contents.swift
+++ ResponseDetective.playground/Contents.swift
@@ -11,21 +11,25 @@ import XCPlayground
 
 // Enable indefinite execution so that requests complete before the playground
 // is killed.
+
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 //: ---
 
 // Start by creating a session configuration that includes ResponseDetective's
 // interception mechanisms.
+
 let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
 ResponseDetective.enableInURLSessionConfiguration(configuration)
 
 // And use the above configuration to create your session instance.
+
 let session = NSURLSession(configuration: configuration)
 
 //: ---
 
 // Now, just create a request and resume its session data task.
+
 let request = NSURLRequest(URL: NSURL(string: "https://httpbin.org/get")!)
 session.dataTaskWithRequest(request).resume()
 
